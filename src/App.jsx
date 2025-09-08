3279b41672091e94b2795ff384f145a1f6a58eb7
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-09-09 01:05:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-semibold text-6xl">
+        Tour Package Booking Management
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
+const MainLayouts = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="max-w-6xl mx-auto">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayouts;
